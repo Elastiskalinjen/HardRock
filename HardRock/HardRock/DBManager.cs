be777774ee1b61a3e9875b9b062733c96a14990b
Elastiskalinjen <sebastian.tvilling@hotmail.com>
2015-05-27 13:10:54 +0200

    LOFaksfsdoåkfdspijf
    HEj

--- HardRock/HardRock/DBManager.cs
+++ HardRock/HardRock/DBManager.cs
@@ -22,15 +22,23 @@ namespace HardRock
                 {
                     conn.Open();
 
-                    using (var cmd = new MySqlCommand("select * from Anstalld", conn))
+                    using (var cmd = new MySqlCommand("select * from Band", conn))
                     {
                         // skriva
-                        cmd.ExecuteNonQuery();
+                        //cmd.ExecuteNonQuery();
 
                         // läsa
                         using (MySqlDataReader reader = cmd.ExecuteReader())
                         {                    
-                                Console.WriteLine(reader.GetValue(0));
+                            while (reader.Read()) {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    Console.Write(reader.GetValue(i) + " ");
+                                    
+                                }
+                                Console.WriteLine(" ");
+                                //reader.GetInt16(7)
+                            }
                         }
 
                     }
@@ -42,19 +50,142 @@ namespace HardRock
             }
         }
 
-        public void Test(string namn)
+        // bra till något kanske
+        public List<List<object>> GetBandInfo()
         {
+            List<List<object>> table = new List<List<object>>();
             try
             {
                 using (var conn = new MySqlConnection(CONNECTION_STRING))
                 {
                     conn.Open();
 
-                    using (var cmd = new MySqlCommand("update Anstalld set Namn=@Namn where AnstId=1", conn))
+                    using (var cmd = new MySqlCommand("select * from Band", conn))
+                    {
+                        // läsa
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                List<object> row = new List<object>();
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    row.Add(reader.GetValue(i));
+                                }
+                                table.Add(row);
+                            }
+                        }
+
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return table;
+        }
+
+        public void InsertAnstalld(int AnstID, string Namn, int PersonNr)
+        {
+            try
+            {
+                using (var conn = new MySqlConnection(CONNECTION_STRING))
+                {
+                    conn.Open();
+                    //  using (var cmd = new MySqlCommand("Insert Band set Namn=@Namn where AnstId=1", conn))
+                    using (var cmd = new MySqlCommand("Insert INTO Anställd VALUE(@AnstID, @Namn, @PersonNr)", conn))
+                    {
+                        // läs på om prepare
+                        cmd.Prepare();
+                        cmd.Parameters.AddWithValue("@AnstID", AnstID);
+                        cmd.Parameters.AddWithValue("@Namn", Namn);
+                        cmd.Parameters.AddWithValue("@PersonNr", PersonNr);
+
+                        // skriva
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void InsertTelefonNr(string TelefonNr, int AnställdID)
+        {
+            try
+            {
+                using (var conn = new MySqlConnection(CONNECTION_STRING))
+                {
+                    conn.Open();
+                    //  using (var cmd = new MySqlCommand("Insert Band set Namn=@Namn where AnstId=1", conn))
+                    using (var cmd = new MySqlCommand("Insert INTO TelefonNr VALUE(@telefonNr, @AnstalldID)", conn))
+                    {
+                        // läs på om prepare
+                        cmd.Prepare();
+                        cmd.Parameters.AddWithValue("@telefonNr", TelefonNr);
+                        cmd.Parameters.AddWithValue("@AnstalldID", AnställdID);
+
+                        // skriva
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void InsertSpelning(int KonsertID, string StartTid, string SlutTid, int ScenID, int BandID)
+        {
+            try
+            {
+                using (var conn = new MySqlConnection(CONNECTION_STRING))
+                {
+                    conn.Open();
+                    //  using (var cmd = new MySqlCommand("Insert Band set Namn=@Namn where AnstId=1", conn))
+                    using (var cmd = new MySqlCommand("Insert INTO Konsert VALUE(@KonserID, @StartTid, @SlutTid, @ScenID, @BandID)", conn))
+                    {
+                        // läs på om prepare
+                        cmd.Prepare();
+                        cmd.Parameters.AddWithValue("@KonserID", KonsertID);
+                        cmd.Parameters.AddWithValue("@StartTid", StartTid);
+                        cmd.Parameters.AddWithValue("@SlutTid", SlutTid);
+                        cmd.Parameters.AddWithValue("@ScenID", ScenID);
+                        cmd.Parameters.AddWithValue("@BandID", BandID);
+
+                        // skriva
+                        cmd.ExecuteNonQuery();
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void InsertBand(int BandID, string Bandnamn, string Genre, int KontaktID, string Land)
+        {
+            try
+            {
+                using (var conn = new MySqlConnection(CONNECTION_STRING))
+                {
+                    conn.Open();
+                    //  using (var cmd = new MySqlCommand("Insert Band set Namn=@Namn where AnstId=1", conn))
+                    using (var cmd = new MySqlCommand("Insert INTO Band VALUE(@BandID, @Bandnamn, @Genre, @KontaktP, @Ursprungsland)", conn))
                     {
                        // läs på om prepare
                         cmd.Prepare();
-                        cmd.Parameters.AddWithValue("@Namn", namn);
+                        cmd.Parameters.AddWithValue("@BandID", BandID);
+                        cmd.Parameters.AddWithValue("@Bandnamn", Bandnamn);
+                        cmd.Parameters.AddWithValue("@Genre", Genre);
+                        cmd.Parameters.AddWithValue("@KontaktP", KontaktID);
+                        cmd.Parameters.AddWithValue("@Ursprungsland", Land);
 
                         // skriva
                         cmd.ExecuteNonQuery();
--- HardRock/HardRock/Form1.cs
+++ HardRock/HardRock/Form1.cs
@@ -12,19 +12,74 @@ namespace HardRock
 {
     public partial class Form1 : Form
     {
+        private DBManager db;
         public Form1()
         {
             InitializeComponent();
+            this.db = new DBManager();
+
+            //var table = db.GetBandInfo();
+
+            //string s = table[0][1] as string;
+
+           
+
+
+
+           // Console.WriteLine(s);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            Setband();
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetKonsert();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SetAnstalld();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SetTelefonNr();
+        } 
         private void Form1_Load(object sender, EventArgs e)
         {
-            new DBManager().Test("' delete Gear");
+           // new DBManager().Test(1,"Dunder björnarna", "Hårdor", 1, "Sagovärlden");
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Setband()
+        {
+            db.InsertBand(int.Parse(textBox1.Text), textBox2.Text, textBox3.Text, int.Parse(textBox4.Text), textBox5.Text);
+        }
+
+        public void SetKonsert()
+        {
+          db.InsertSpelning(int.Parse(textBox10.Text), textBox9.Text, textBox8.Text, int.Parse(textBox7.Text), int.Parse(textBox6.Text));
+        }
+
+        public void SetAnstalld()
+        {
+           db.InsertAnstalld(int.Parse(textBox13.Text), textBox12.Text, int.Parse(textBox11.Text));
+        }
+
+        public void SetTelefonNr()
+        {
+            db.InsertTelefonNr(textBox14.Text, int.Parse(textBox15.Text));
+        }
+
+
+          
+
+        
+        
     }
 }
